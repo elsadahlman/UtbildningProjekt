63fa92a40288c28fdb7d34acc30ab83b8117cede
Carin Ivarsson <Carin.Ivarsson@skandia.se>
2023-09-27 13:53:09 +0200

    video bg

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -79,8 +79,8 @@ const App = () => {
         <UserContextProvider>  
             {loading  && "Loading...."}
                 <div className="video-container">
-            <iframe src="https://www.youtube.com/embed/rUsyqGED8CE?&controls=0&autoplay=1&mute=1&playsinline=1" ></iframe>
-            </div>
+                    <iframe src="https://www.youtube.com/embed/rUsyqGED8CE?&controls=0&autoplay=1&mute=1&playsinline=1" ></iframe>
+                </div>
             <StyledApp className="app-over-video">
                 <h1>Companies</h1>
                 <LoginButton/>
