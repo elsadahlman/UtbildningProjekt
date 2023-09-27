2e51b4abb4ce9495787f896cab47541a2b586aae
Carin Ivarsson <Carin.Ivarsson@skandia.se>
2023-09-27 17:03:19 +0200

    ..

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -17,46 +17,48 @@ const App = () => {
     const [loading, setLoading] = useState<boolean>(false);
 
     const onClickDelete = async (id:string) =>{
-
+        setLoading(true);
         try {
             await deleteCompanyById(id);
             await fetchCompanies();
         } catch (e) {
             console.log('Error when deleting company.', e)
         }
-
+        setLoading(false);
     }
 
     const fetchCompanies = useCallback(async () => {
-
+        setLoading(true);
         try {
             const newCompanies = await getCompanies();
             setCompanies(newCompanies);
         } catch (e) {
             console.log('Error when posting new company', e)
         }
-
+        setLoading(false);
 
     }, [])
 
     const onClickUpdate = async (id: string, data: CompanyForUpdate) =>{
+        setLoading(true);
         try {
             await updateCompanyById(id, data);
             await fetchCompanies();
         } catch (e) {
             console.log('Error when updating company.', e)
         }
+        setLoading(false);
     }
 
     const onClickCreate = async (data: CompanyForCreation) =>{
-
+        setLoading(true);
         try {
             await postCompany(data);
             await fetchCompanies();
         } catch (e) {
             console.log('Error when posting new company.', e)
         }
-
+        setLoading(false);
     }
 
     
@@ -78,7 +80,7 @@ const App = () => {
 // Man använder sin provider för att wrappa och då kommer de åt den aktuella contexten. 
     return (
         <UserContextProvider>  
-            {loading  && "Loading...."}
+            {/* {loading  && "Loading...."} */}
                 <div className="video-container">
                     <iframe src="https://www.youtube.com/embed/aIXb-RPb358?start=72&controls=0&autoplay=1&mute=1&playsinline=1&loop=1" ></iframe>  
                 </div>
